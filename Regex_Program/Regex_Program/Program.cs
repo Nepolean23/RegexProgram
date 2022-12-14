d908249683ff7dbbd4bd0bd0f8b9c0f0a276133f
Nepolean23 <nlean1996@gmail.com>
2022-12-14 14:09:19 +0530

    Code For E-Mail Id in Regex

--- Regex_Program/Regex_Program/Program.cs
+++ Regex_Program/Regex_Program/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Regex_Program;
 
 //UC_1_FirstName FirstName = new UC_1_FirstName();
-UC_2_LastName LastName= new UC_2_LastName();
- if(LastName.ValidatePincode(""))
+//UC_2_LastName LastName= new UC_2_LastName();
+UC_3_Email_Id Id= new UC_3_Email_Id();
+
+ if(Id.ValidatePincode("abc.xyz@bl.co.in"))
 {
     Console.WriteLine("Valid");
 }
---
+++ Regex_Program/Regex_Program/UC_3-Email_Id.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Program
+{
+    public class UC_3_Email_Id
+    {
+        public static  string Regex_rule = "^[A-Za-z]+[.]([A-Za-z]+)?[@][A-Za-z]+[.][A-Za-z]+[.]([A-Za-z]+)?$";
+
+        public bool ValidatePincode(string inputstring)
+        {
+                return Regex.IsMatch(inputstring, Regex_rule);
+        }
+    }
+}
