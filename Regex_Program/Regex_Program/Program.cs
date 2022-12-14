bfe74cc7a71f7a768dd4139851d1c8ee30a018d3
Nepolean23 <nlean1996@gmail.com>
2022-12-14 20:18:08 +0530

    Code for Regex in PhoneNumber

--- Regex_Program/Regex_Program/Program.cs
+++ Regex_Program/Regex_Program/Program.cs
@@ -2,9 +2,10 @@
 
 //UC_1_FirstName FirstName = new UC_1_FirstName();
 //UC_2_LastName LastName= new UC_2_LastName();
-UC_3_Email_Id Id= new UC_3_Email_Id();
+//UC_3_Email_Id Id= new UC_3_Email_Id();
+UC_4_PhoneNumber Number=new UC_4_PhoneNumber();
 
- if(Id.ValidatePincode("abc.xyz@bl.co.in"))
+ if(Number.ValidatePincode(""))
 {
     Console.WriteLine("Valid");
 }
