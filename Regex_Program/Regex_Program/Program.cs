f3dd017972026b61ff87f99516fb8e6472b4d0a5
Nepolean23 <nlean1996@gmail.com>
2022-12-14 12:52:30 +0530

    Regex Code For FirstName

--- Regex_Program/Regex_Program/Program.cs
+++ Regex_Program/Regex_Program/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Regex_Program;
+
+UC_1_FirstName FirstName = new UC_1_FirstName();
+ if(FirstName.validatePincode(""))
+{
+    Console.WriteLine("Valid");
+}
+ else
+{
+    Console.WriteLine("Invalid");
+}
+
+
---
+++ Regex_Program/Regex_Program/UC_1-FirstName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Program
+{
+    public class UC_1_FirstName
+    {
+        public static string Regex_Rule = "^[A-Za-z]{3,}";
+
+        public bool validatePincode(string inputstring)
+        {
+            return Regex.IsMatch(inputstring, Regex_Rule);
+        }
+
+    }
+}
