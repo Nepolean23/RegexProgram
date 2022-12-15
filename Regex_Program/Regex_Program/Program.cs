cbe99cb3c2787e8283a1b39b4d80f46a03ea63a7
Nepoleanvijayakumar <114413815+Nepolean23@users.noreply.github.com>
2022-12-15 15:42:25 +0530

    Update Program.cs

--- Regex_Program/Regex_Program/Program.cs
+++ Regex_Program/Regex_Program/Program.cs
@@ -1,13 +1,19 @@
 ﻿using Regex_Program;
 
 //UC_1_FirstName FirstName = new UC_1_FirstName();
+//Console.WriteLine(password.ValidatePincode("Nepolean"));
 
 //UC_2_LastName LastName= new UC_2_LastName();
+//Console.WriteLine(password.ValidatePincode("Vijayakumar"));
 
 //UC_3_Email_Id Id= new UC_3_Email_Id();
+//Console.WriteLine(password.ValidatePincode("abc.xyz@bl.co.in"));
+
 //UC_4_PhoneNumber Number=new UC_4_PhoneNumber();
-UC_5_PasswordCreating password= new UC_5_PasswordCreating();
-Console.WriteLine(password.ValidatePincode("Qsrdgv*8"));
+//Console.WriteLine(password.ValidatePincode("91 9943177412"));
+
+//UC_5_PasswordCreating password= new UC_5_PasswordCreating();
+//Console.WriteLine(password.ValidatePincode("Qsrdgv*8"));
 
 
   
