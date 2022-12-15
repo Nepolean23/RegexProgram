ef39fb09b644e7200d49dfa965f88b2ad2129efc
Nepolean23 <nlean1996@gmail.com>
2022-12-15 21:05:13 +0530

    Code For Testing of SampleEmails in Regex

--- Regex_Program/Regex_Program/Program.cs
+++ Regex_Program/Regex_Program/Program.cs
@@ -4,8 +4,10 @@
 //UC_2_LastName LastName= new UC_2_LastName();
 //UC_3_Email_Id Id= new UC_3_Email_Id();
 //UC_4_PhoneNumber Number=new UC_4_PhoneNumber();
-UC_5_PasswordCreating password= new UC_5_PasswordCreating();
-Console.WriteLine(password.ValidatePincode("Qsrdgv*8"));
+//UC_5_PasswordCreating password= new UC_5_PasswordCreating();
+UC_9_SampleEmailId Sample=new UC_9_SampleEmailId();
+Console.WriteLine(Sample.ValidatePincode("abc+100@gmail.com"));
+
 
 
   
--- Regex_Program/Regex_Program/UC_9-SampleEmailId.cs
+++ Regex_Program/Regex_Program/UC_9-SampleEmailId.cs
@@ -7,16 +7,14 @@ using System.Threading.Tasks;
 
 namespace Regex_Program
 {
-     public class UC_5_PasswordCreating
+    public class UC_9_SampleEmailId
     {
         public static string Regex_rule = "^[a-z]{3}([.+-@])?([a-z]+)?([0-9]+)?([@])?([a-z]+)?[.][a-z]{3}([,.])?([a-z]+)?$";
 
         public bool ValidatePincode(string inputstring)
         {
-           return Regex.IsMatch(inputstring, Regex_rule);
+            return Regex.IsMatch(inputstring,Regex_rule);
         }
 
-        
-        
     }
 }
