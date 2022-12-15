0298312a20938b908fd9c61b4a944e4c3575396a
Nepolean23 <nlean1996@gmail.com>
2022-12-15 15:33:16 +0530

    Code for Password Creating in Regex

---
+++ Regex_Program/Regex_Program/UC_5-PasswordCreating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Program
+{
+     public class UC_5_PasswordCreating
+    {
+        public const string Regex_rule = "(^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,})$";
+
+        public bool ValidatePincode(string inputstring)
+        {
+           return Regex.IsMatch(inputstring, Regex_rule);
+        }
+
+        
+    }
+}
